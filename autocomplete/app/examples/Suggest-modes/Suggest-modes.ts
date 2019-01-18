aff908ec74f4d526dd125fad91769973175596ff
NSBuildUser <nsbuilduser@mcsofnsbuild08.telerik.com>
2019-01-18 14:55:57 +0200

    chore: update 'autocomplete' demo for nativescript-ui-autocomplete v3.11.0

--- autocomplete/app/examples/Suggest-modes/Suggest-modes.ts
+++ autocomplete/app/examples/Suggest-modes/Suggest-modes.ts
@@ -15,11 +15,12 @@ export default {
       <NavigationButton text="Back" android.systemIcon="ic_menu_back" @tap="onNavigationButtonTap"></NavigationButton>
     </ActionBar>
     <ListView for="example in examples"
+              separatorColor="transparent"
               @itemTap="goToExample">
       <v-template>
         <StackLayout class="item" orientation="vertical">
-          <Label :text="example.description">
-          </Label>
+          <Label :text="example.description" class="titleLabel"></Label>
+          <StackLayout height="1" backgroundColor="#EEEEEE"></StackLayout>
         </StackLayout>
       </v-template>
     </ListView>
