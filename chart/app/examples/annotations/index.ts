57b830edce7b1443275cc795dba063d6f904b65a
NSBuildUser <nsbuilduser@mcsofnsbuild08.telerik.com>
2019-01-18 15:23:41 +0200

    chore: update 'chart' demo for nativescript-ui-chart v3.11.1

--- chart/app/examples/annotations/index.ts
+++ chart/app/examples/annotations/index.ts
@@ -15,11 +15,12 @@ export default {
     </ActionBar>
     <ListView for="example in examples"
               class="list"
+              separatorColor="transparent"
               @itemTap="goToExample">
       <v-template>
-        <StackLayout class="item" orientation="vertical" style="margin-top: 20">
-          <Label :text="example.description">
-          </Label>
+        <StackLayout class="item" orientation="vertical">
+          <Label :text="example.description" class="titleLabel"></Label>
+          <StackLayout height="1" backgroundColor="#EEEEEE"></StackLayout>
         </StackLayout>
       </v-template>
     </ListView>
--- chart/app/examples/interaction/index.ts
+++ chart/app/examples/interaction/index.ts
@@ -19,11 +19,12 @@ export default {
     <StackLayout class="page">
       <ListView for="example in examples"
                 class="list"
+                separatorColor="transparent"
                 @itemTap="goToExample">
         <v-template>
-          <StackLayout class="item" orientation="vertical" style="margin-top: 20">
-            <Label :text="example.description">
-            </Label>
+          <StackLayout class="item" orientation="vertical">
+            <Label :text="example.description" class="titleLabel"></Label>
+            <StackLayout height="1" backgroundColor="#EEEEEE"></StackLayout>
           </StackLayout>
         </v-template>
       </ListView>
